7bdb21c1f19ef662847826eb53e8185b6cc4fc25
MaximShovkun <maximshovkun95@gmail.com>
2019-10-06 15:21:32 +0300

    reorganization

--- tweet_dumper.py
+++ tweet_dumper.py
@@ -51,7 +51,7 @@ def get_all_tweets(screen_name):
     outtweets = [[tweet.id_str, tweet.created_at, tweet.full_text] for tweet in alltweets]
 
     # write the csv
-    with open('%s_tweets.csv' % screen_name, 'w') as f:
+    with open('data/%s_tweets.csv' % screen_name, 'w') as f:
         writer = csv.writer(f)
         writer.writerow(["id", "created_at", "text"])
         writer.writerows(outtweets)
